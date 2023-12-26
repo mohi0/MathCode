e1bbbc100e9617625e4b0c2e2b81ea88e943d466
mohi0 <88849405+mohi0@users.noreply.github.com>
2023-12-27 00:41:32 +0530

    Create Matrix.h
    Defines structures to be used

---
+++ Matrix.h
@@ -0,0 +1,43 @@
+struct matrix2x2{
+    double matrix[2][2];
+    char name[30];
+};
+struct matrix3x2{
+    double matrix[3][2];
+    char name[30];
+};
+struct matrix3x3{
+    double matrix[3][3];
+    char name[30];
+};
+
+struct matrix2x3{
+    double matrix[2][3];
+    char name[30];
+};
+
+struct matrix3x4{
+    double matrix[3][4];
+    char name[30];
+};
+
+struct matrix4x4{
+    double matrix[4][4];
+    char name[30];
+};
+
+struct matrix5x5{
+    double matrix[5][5];
+    char name[30];
+};
+
+struct matrix6x6{
+    double matrix[6][6];
+    char name[30];
+};
+
+struct matrix7x7{
+    double matrix[7][7];
+    char name[30];
+};
+
