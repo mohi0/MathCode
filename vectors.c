0ce9f2cffca6c7b36bb93a80eacf85ef96a6438a
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-06-14 10:37:30 +0530

    added functions for operations on vectors
    addition, subtraction, dotProduct of vectors

--- vectors.c
+++ vectors.c
@@ -1,2 +1,27 @@
 #include<stdio.h>
 #include<math.h>
+
+
+typedef struct v3{
+    float i, j, k;
+} v3;
+
+void addv3s(v3 *a, v3 *b, v3 *result){
+    result->i = a->i + b->i;
+    result->j = a->j + b->j;
+    result->k = a->k + b->k;
+}
+
+void subv3s(v3 *a, v3 *b, v3 *result){
+    result->i = a->i - b->i;
+    result->j = a->j - b->j;
+    result->k = a->k - b->k;
+}
+
+void dotv3s(v3 *a, v3 *b, float *result){
+    *result = (a->i * b->i) + (a->j * b->j) + (a->k * b->k);
+}
+
+void printv3(v3 *v){
+    printf("\n %fi %fj %fk\n", v->i, v->j, v->k);
+}
