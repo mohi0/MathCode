1af01f5e92b9f38b014d4d5cba8d5899cdcb3660
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-09-09 21:07:53 +0530

    Update vectors.c
    added function to calculate angle between two vectors

--- vectors.c
+++ vectors.c
@@ -43,6 +43,13 @@ void crossv3s(v3 *a, v3 *b, v3 *result){
     result->k = (a->i * b->j) - (a->j * b->i);
 }
 
+void anglev3s(v3 *a, v3 *b, float *result){
+    float dotProd, magnitudeA, magnitudeB; 
+    dotv3s(a, b, &dotProd);
+    magnitudev3(a, &magnitudeA); magnitudev3(b, &magnitudeB);
+    *result = acosf(dotProd / (magnitudeA * magnitudeB));
+}
+
 v3 meanv3s(v3 *vectors, int count){
     v3 mean = {0, 0, 0};
     for (int i = 0; i < count; i++){
