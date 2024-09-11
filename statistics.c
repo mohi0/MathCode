346f1e70adc30e55378f2452d6f865396758fa77
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-09-11 19:34:07 +0530

    Update statistics.c
    added median function

--- statistics.c
+++ statistics.c
@@ -1,8 +1,17 @@
-float average(float *nums, count){
+float average(float *nums, int count){
   float sum = 0.0f;
   for(int i = 0; i < count; i++){
     sum += *(nums + i);
   }
-  return sum / count
+  return sum / count;
 
+}
+
+float median(float *sortedArr, int count){
+    float median;
+    if (count  % 2 == 0)
+    {
+        median =  ( *(sortedArr + count / 2 - 1) + *(sortedArr + count / 2) ) / 2;
+    }
+    return median;
 }
