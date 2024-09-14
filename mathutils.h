991c25939d4a6f16777d33f951332bec12d83249
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-09-14 18:10:22 +0530

    Update mathutils.h
    added functions to calculate nth root and geometric mean of numbers

--- mathutils.h
+++ mathutils.h
@@ -10,3 +10,16 @@ float numsRMS(float nums[], int count){
     }
     return sqrtf(sum / count);
 }
+
+float nthRoot(float value, int root){
+    return pow(value, 1.0 / root);
+}
+
+float geoMean(float nums[], int count){
+    float prod = 1;
+    for (int i = 0; i < count; i++)
+    {
+        prod  *= nums[i];
+    }
+    return nthRoot(prod, count);
+}
