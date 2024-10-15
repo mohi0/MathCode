554c8eb7df22d7dfa5e2f8e74bb82eb994a23edd
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-10-15 23:18:31 +0530

    Update math_structures.h
    added basic Table structure representation

--- math_structures.h
+++ math_structures.h
@@ -1,4 +1,15 @@
 // file for defining math structures 
 
+typedef struct HeaderCol {
+  Cell *cell;
+  HeaderfCol *nextHeader;
+} HeaderCol;
+
+typedef struct Cell {
+  Cell *nextInCol;
+  Void *data;
+} Cell;
+
 typedef struct Table {
+  HeaderCol *start;
 } Table;
