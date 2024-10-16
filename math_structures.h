3a1684cf439468b6b3252e4694881cba8af9a036
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-10-16 21:45:36 +0530

    Update math_structures.h
    added basic functions

--- math_structures.h
+++ math_structures.h
@@ -7,9 +7,38 @@ typedef struct HeaderCol {
 
 typedef struct Cell {
   Cell *nextInCol;
-  Void *data;
+  void *data;
 } Cell;
 
 typedef struct Table {
   HeaderCol *start;
 } Table;
+
+//initialise a new table
+void newTable(){
+}
+
+
+// add a column to table
+void addCol(){
+}
+
+// add a row to table
+void addRow(){
+}
+
+// remove a column ffrom table
+void removeCol(){
+}
+
+// remove a row from the table
+void removeRow(){
+}
+
+//get column data from some index range or entire column
+void getColData(){
+}
+
+//get entire row data from some index range or entire row
+void getRowData(){
+}
