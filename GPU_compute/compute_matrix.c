20c618012ab5ac13331f08ab928d8dc6c5cd3310
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-11-18 21:15:22 +0530

    Update compute_matrix.c
    added code for Vulkan GPI API compute shader

--- GPU_compute/compute_matrix.c
+++ GPU_compute/compute_matrix.c
@@ -1,5 +1,77 @@
-#include <stdio.h>
+//needs testing
 
+#include <stdio.h>
+#include <vulkan/vulkan.h>
 
 int prepareData(float *dataArr){
+  VkInstance instance;
+  VkApplicationInfo appInfo = {
+      .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+      .pApplicationName = "Matrix",
+      .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+      .pEngineName = "Nil",
+      .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+      .apiVersion = VK_API_VERSION_1_3   
+  };
+  VkInstanceCreateInfo createInfo = {
+    .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+    .pApplicationInfo = &appInfo,   
+  };
+  uint32_t deviceCount = 0;
+  vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+  std::vector<VkPhysicalDevice> physicalDevices(deviceCount);
+  vkEnumeratePhysicalDevices(instance, &deviceCount, physicalDevices.data());   
+
+  VkPhysicalDevice physicalDevice = physicalDevices[0];
+
+  VkDevice device;
+  VkDeviceCreateInfo deviceCreateInfo = {
+      .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO
+  };
+
+  vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device);
+  
+  VkDeviceQueueCreateInfo queueCreateInfo = {};
+  queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+  queueCreateInfo.queueFamilyIndex = queueFamilyIndex;
+  queueCreateInfo.queueCount = 1;
+  queueCreateInfo.pQueuePriorities = &queuePriority;
+  
+  VkPhysicalDeviceFeatures deviceFeatures = {};
+  
+  VkDeviceCreateInfo createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+  createInfo.queueCreateInfoCount = 1;
+  createInfo.pQueueCreateInfos = &queueCreateInfo;
+  createInfo.pEnabledFeatures = &deviceFeatures;
+  createInfo.enabledExtensionCount = 0;
+  createInfo.ppEnabledExtensionNames = nullptr;
+  
+  vkCreateDevice(physicalDevice, &createInfo, nullptr, &device);
+  
+  vkGetDeviceQueue(device, queueFamilyIndex, 0, &computeQueue);
+
+  
+  VkShaderModuleCreateInfo createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+  createInfo.codeSize = codeSize;
+  createInfo.pCode = code;
+  
+  vkCreateShaderModule(device, &createInfo, nullptr, &computeShaderModule);
+
+  VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
+  pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+  pipelineLayoutInfo.setLayoutCount = 0;
+  pipelineLayoutInfo.pSetLayouts = nullptr;
+  pipelineLayoutInfo.pushConstantRangeCount = 0;
+  pipelineLayoutInfo.pPushConstantRanges = nullptr;
+  
+  vkCreatePipelineLayout(device,   
+  &pipelineLayoutInfo, nullptr, &pipelineLayout);
+
+  VkPipelineShaderStageCreateInfo shaderStageInfo = {};
+  shaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+  shaderStageInfo.stage = VK_SHADER_STAGE_COMPUTE_BIT;
+  shaderStageInfo.module = computeShaderModule
+  
 }
