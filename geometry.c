8123dfeac29207ec7bf0f9cd93f4f552e45f12a8
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-12-17 21:54:25 +0530

    Update geometry.c
    added midpoint function and made compatible with math utils

--- geometry.c
+++ geometry.c
@@ -1,14 +1,12 @@
 // for geometry related functions
 #include<math.h>
-typedef struct PointCartesian2D {
-  float x, y;
-};
+#include "mathutils.h"
 
-float distanceCart2D(PointCartesian2D p1, PointCartesian2D p2){
+float Cartesian2D(Cartesian2D p1, Cartesian2D p2){
   return sqrtf(powf(p2.x - p1.x) + powf(p2.y - p2.1));
 }
 
-PointCartesian2D midPointCart2D(PointCartesian2D p1, PointCartesian2D p2){
+Cartesian2D midPointCart2D(Cartesian2D p1, Cartesian2D p2){
   PointCartesian2D mp = {};
   mp.x = (p1.x + p2.x) / 2;
   mp.y = (p1.y + p2.y) / 2;
