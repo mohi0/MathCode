4b3f97a441f307c6e1d0350a6b8a1a52739874e4
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-12-17 21:51:15 +0530

    Update geometry.c
    added function for mid point of two points in cartesian 2D points

--- geometry.c
+++ geometry.c
@@ -4,7 +4,14 @@ typedef struct PointCartesian2D {
   float x, y;
 };
 
-float distance(PointCartesian2D p1, PointCartesian2D p2){
+float distanceCart2D(PointCartesian2D p1, PointCartesian2D p2){
   return sqrtf(powf(p2.x - p1.x) + powf(p2.y - p2.1));
+}
+
+PointCartesian2D midPointCart2D(PointCartesian2D p1, PointCartesian2D p2){
+  PointCartesian2D mp = {};
+  mp.x = (p1.x + p2.x) / 2;
+  mp.y = (p1.y + p2.y) / 2;
+  return mp;
 }
 
