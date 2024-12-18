60ec6e67ce6a0f5bc6a3250dd00664b82bff4e52
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-12-18 18:37:49 +0530

    Update geometry.c
    midpoint of points in 3D

--- geometry.c
+++ geometry.c
@@ -7,7 +7,7 @@ float distanceCart2D(Cartesian2D p1, Cartesian2D p2){
 }
 
 Cartesian2D midPointCart2D(Cartesian2D p1, Cartesian2D p2){
-  PointCartesian2D mp = {};
+  Cartesian2D mp = {};
   mp.x = (p1.x + p2.x) / 2;
   mp.y = (p1.y + p2.y) / 2;
   return mp;
@@ -16,4 +16,13 @@ Cartesian2D midPointCart2D(Cartesian2D p1, Cartesian2D p2){
 
 float distanceCart3D(Cartesian3D p1, Cartesian3D p2){
   return sqrtf(powf(p2.x - p1.x, 2) + powf(p2.y - p1.y, 2) + powf(p2.z - p1.z, 2));
+}
+
+
+Cartesian3D midPointCart3D(Cartesian3D p1, Cartesian3D p2){
+  Cartesian3D mp = {};
+  mp.x = (p1.x + p2.x) / 2;
+  mp.y = (p1.y + p2.y) / 2;
+  mp.z = (p1.z + p2.z) / 2;
+  return mp;
 }
