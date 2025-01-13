0f08c6a7e581262989f48eca9bae95b32f519294
mohi0 <88849405+mohi0@users.noreply.github.com>
2025-01-13 23:45:42 +0530

    Update math_structures.h
    The math structures data / code content model is changed for a more generalised approach

--- math_structures.h
+++ math_structures.h
@@ -1,48 +1,6 @@
 // file for defining math structures 
 
-typedef struct HeaderCol {
-  Cell *cell;
-  HeaderfCol *nextHeader;
-} HeaderCol;
-
-typedef struct Cell {
-  Cell *nextInCol;
-  void *data;
-} Cell;
-
-typedef struct Row {
+typedef struct Tensor {
+  int num_dimensions;
   
-} Row;
-
-typedef struct Table {
-  HeaderCol *start;
-} Table;
-
-//initialise a new table
-Table newTable(){
-}
-
-
-// add a column to table
-void addCol(Table &table){
-}
-
-// add a row to table
-void addRow(){
-}
-
-// remove a column from table
-void removeCol(){
-}
-
-// remove a row from the table
-void removeRow(){
-}
-
-//get column data from some index range or entire column
-void getColData(){
-}
-
-//get entire row data from some index range or entire row
-void getRowData(){
-}
+};
