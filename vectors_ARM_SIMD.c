2fb0fef9aaa37d01292dcdca074b2682c44f15b6
mohi0 <88849405+mohi0@users.noreply.github.com>
2025-07-17 15:39:14 +1000

    Update vectors_ARM_SIMD.c
    SIMD reduction sum

--- vectors_ARM_SIMD.c
+++ vectors_ARM_SIMD.c
@@ -1,1 +1,8 @@
 //code for SIMD vector operations on ARM Microprocessors 
+
+// FOR ARM CORTEX Processors with support for NEON SIMD only
+
+float simdSum(const float* arr, int n){
+  float32x4_t vecSum = vdupq_n_f32(0.0f); 
+  
+}
